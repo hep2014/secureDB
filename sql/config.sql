3242802a8d0f0f40e1b78d77ae695276b0bf72ee
Ilya Alexandrovich Matveev <409097@niuitmo.ru>
2025-11-27 12:49:08 +0300

    create config file

---
+++ sql/config.sql
@@ -0,0 +1,5 @@
+DB_NAME = "timpLab6" # Выберите любое название 
+DB_HOST = "localhost" # БД запущена локально 
+DB_USER = "postgres" # БД запущена со стандартным пользователем
+DB_PORT = "5432" 
+DB_PASSWORD = "postgres" # Вставьте свой пароль пользователя
